2918d8079c899cc614bd4c9c31797384337b9df3
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-13 17:17:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var add = function add(a, b) {
+function add(a, b) {
   return a + b;
-};
+}
 
-var subtract = function subtract(a, b) {
+function subtract(a, b) {
   return a - b;
-};
+}
 
-var multiply = function multiply(a, b) {
+function multiply(a, b) {
   return a * b;
-};
+}
 
-var divide = function divide(a, b) {
+function divide(a, b) {
   return a / b;
-};
+}
 
-var inc = function inc(n) {
+function inc(n) {
   return ++n;
-};
+}
 
-var dec = function dec(n) {
+function dec(n) {
   return --n;
-};
+}
 
-var makeInt = function makeInt(n) {
+function makeInt(n) {
   return parseInt(n, 10);
-};
+}
 
-var preserveDecimal = function preserveDecimal(n) {
+function preserveDecimal(n) {
   return parseFloat(n);
-};
+}
